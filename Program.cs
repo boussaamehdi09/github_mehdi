9b9dc1227b52f4d5ac203f5b0fe254f30aa0a1d2
boussaamehdi09 <boussaa.mehdi@franchettisalviani.net>
2025-10-23 10:08:46 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace github_mehdi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
